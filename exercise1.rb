fdcb6b3e5833569f19e93114b9eb51f3e7f583ea
Roopesh <rrattan.89@gmail.com>
2017-05-15 16:02:40 -0400

    Exercise 2

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,13 @@
 puts 2
 puts 3
 puts 2 + 3
+
+puts 'Hello world'
+puts "Hello world"
+
+puts "Ada Lovelace lived for #{1852 - 1815} years."
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+# Calculating tip for a meal
+puts 55*0.2
+
+# String + Integer
+puts "ABC"
+puts 7
+puts "ABC" + 7.to_s
+
+# String Interpolation
+puts "There are #{45628 * 7839} spiders in your room"
+
+# Calculating value of an expression
+puts (10 < 20 && 30 < 20) || !(10 == 11)
