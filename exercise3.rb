104516dff873055b067e00af7294df3b4e04bcbc
Roopesh <rrattan.89@gmail.com>
2017-05-15 16:19:02 -0400

    What is your name?

---
+++ exercise3.rb
@@ -0,0 +1,9 @@
+# Practice
+name = "Sandra"
+greeting = "Hello #{name}! It's good to see you again."
+mission = "Your mission, should you choose to accept it..."
+
+puts greeting + mission
+
+# Actual Exercise
+puts "What is your name"
